13f8c77eaecae61c3944d0444441f04a20df3caa
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-07-04 12:19:03 -0300

    pokelista

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,14 @@ export default function App() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 190,
+    height:400,
     bgcolor: 'background.paper',
     border: '1px solid #000',
     borderRadius: '20px',
     boxShadow: 24,
     p: 4,
+
   };
     const [nome,setNome]=useState('')
     const[id,setId]=useState('')
@@ -31,6 +33,7 @@ export default function App() {
     const [peso,setPeso]=useState('')
     const [altura,setAltura]=useState('')
     const [pokeBusca,setPokebusca]=useState('1')
+    const [fundoImg, setFundoImg]= useState('')
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeBusca}`)
         .then(function (response) {
             setNome(response.data.name)
@@ -40,10 +43,16 @@ export default function App() {
             setPeso(response.data.weight)
             setAltura(response.data.height)
             console.log(response.data)
+            if(tipo=='fire'){
+              setFundoImg('https://www.publicdomainpictures.net/pictures/400000/velka/marmoriert-rot-hintergrund-textur.jpg')
+            }else if(tipo=='electric'){
+              setFundoImg('https://st.depositphotos.com/1605581/4781/i/450/depositphotos_47819249-stock-illustration-%20abstract-yellow-background-texture-design.jpg')
+            }
         })
         .catch(function (error) {
             console.log(error);
         })
+
   return (
     <>
       <Titulo/>
@@ -56,13 +65,13 @@ export default function App() {
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
+          > 
+            <Box sx={style} style={{backgroundImage:`URL(${fundoImg})`}}>
               <Typography id="modal-modal-title" variant="h6" component="h2">
               <h4>{nome}({id})</h4>
               </Typography>
               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              <img style={{ width: "130px" }} src={imagem} />
+              <img style={{ width: "230px", padding:0, margin:0 }} src={imagem} />
               <h5>Tipo:{tipo}</h5>
               <h5>Altura:{altura}</h5>
               <h5>Peso:{peso}</h5>
